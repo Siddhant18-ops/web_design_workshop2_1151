eb60e12b0dc4fcd1d9965265df10edf0dd8307e8
siddhant18-ops <siddhantkashyap264@gmail.com>
2026-04-09 14:10:36 +0530

    initial commit

---
+++ javascript/js fundamentals/app.js
@@ -0,0 +1,14 @@
+var name ="siddhant";
+var age = 30;
+var isemployed = true;
+console.log(name);
+console.log(age);
+console.log(isemployed);
+// var age =50;
+// console.log(age);
+// why this is dangerous;
+if(true){
+    var age =30;
+}
+console.log(age);
+ 
