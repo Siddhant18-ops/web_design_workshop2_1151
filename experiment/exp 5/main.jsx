ea2ddcc5cf156880de1a7fd1b89bea854986e42f
siddhant18-ops <siddhantkashyap264@gmail.com>
2026-05-22 11:23:40 +0530

    5th experiment manipulation

---
+++ experiment/exp 5/main.jsx
@@ -0,0 +1,55 @@
+let employees=[];
+
+function addEmployee(){
+
+let name=document.getElementById("name").value;
+
+let empId=document.getElementById("empId").value;
+
+let salary=parseFloat(
+document.getElementById("salary").value
+);
+
+let dept=document.getElementById("dept").value;
+
+if(name===""||empId===""||
+isNaN(salary)||dept===""){
+
+alert("Please fill all fields");
+
+return;
+}
+
+let employee={
+
+name:name,
+id:empId,
+salary:salary,
+department:dept
+
+};
+
+employees.push(employee);
+
+alert("Employee Added Successfully");
+}
+
+function displayEmployees(){
+
+let output="<h3>All Employees</h3>";
+
+employees.forEach(emp=>{
+
+output+=`
+Name:${emp.name}
+ID:${emp.id}
+Salary:${emp.salary}
+Dept:${emp.department}<br>
+`;
+
+});
+
+document.getElementById(
+"output"
+).innerHTML=output;
+}
